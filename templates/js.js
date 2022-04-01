3936999094088f64738c68e8fd8acef6c78ea0d1
MissCrispenCakes <10444589+MissCrispenCakes@users.noreply.github.com>
2022-04-01 18:31:00 -0400

    reformat

---
+++ templates/js.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var p = document.getElementById("mydata");
+    //call a sub-algorithm to return the current time and date in a specific format
+    p.innerHTML = getDateInCustomizedFormat();
+}
+  
+  
+document.getElementById("output").innerHTML = "Sound"
+document.getElementById("output").innerHTML = "Video"
